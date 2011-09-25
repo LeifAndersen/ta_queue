a44b07c3021ac4930c46c0cbe5e49d7981a4665b
Parker Wightman <parkerwightman@gmail.com>
2011-09-24 21:34:45 -0600

    Fixing more login bugs

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,7 @@
 class BoardsController < ApplicationController
   respond_to :xml, :json, :html
   before_filter :get_board
+  before_filter :redirect_if_logged_in, :only => [:login, :login_user]
 
   def show
     @current_user = User.find(session['user_id'])
@@ -17,7 +18,6 @@ class BoardsController < ApplicationController
   end
 
   def login
-
   end
 
   def logout
@@ -28,25 +28,17 @@ class BoardsController < ApplicationController
       user_id = params[:id]
     end
 
-    @user = User.find(user_id)
+    @user = User.where(:id => user_id).first
     respond_with do |f|
       if @user
         @user.destroy
+      end
         f.html { redirect_to board_login_path }
         f.json { render head, :status => :success }
-      end
     end
   end
 
   def login_user
-    # If the person is already logged in, just send them along
-    if session["user_id"]
-      if User.where(:id => session["user_id"]).first
-        redirect_to @board and return
-      else
-        session.delete "user_id"
-      end
-    end
 
     if params[:ta_password]
       if params[:ta_password] != @board.password
@@ -83,6 +75,15 @@ class BoardsController < ApplicationController
         @board = Board.where(:title => params[:board_id]).first
       end
         
+    end
+    def redirect_if_logged_in
+      if session["user_id"]
+        if User.find(session["user_id"])
+          redirect_to @board and return
+        else
+          session.delete "user_id"
+        end
+      end
     end
 
 end
