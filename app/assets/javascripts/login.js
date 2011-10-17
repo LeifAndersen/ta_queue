6a7aa14dee85fcb5a977f74ec9a710a1a28c99ef
Andrew Butterfield <andrewcbutterfield@gmail.com>
2011-10-17 07:35:16 -0600

    Fixed login page functionality. Should be good to go.

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -16,10 +16,20 @@ $(document).ready(function()
   $('#ta_username,#student_username').attr('value','username');
   $('#student_location').attr('value','location');
   
-  if (window.location.search == 'ta')
+  if (window.location.search == '?ta=true')
   {
-    alert('gotcha');
+    $('#ta_panel').show();
+    $('#student_panel').hide();
+    turnOnTaTab();
   }
+  
+  if (window.location.search == '?student=true')
+  {
+    $('#ta_panel').hide();
+    $('#student_panel').show();
+    turnOnStudentTab();
+  }
+  
 	// TA tab/panel and student tab/panel toggle
 	$('#student_tab, #ta_tab')
 	  .click(function ()
@@ -27,169 +37,176 @@ $(document).ready(function()
 
   	  if ($(this).attr('id') == 'student_tab' && $('#student_panel').css('display') == 'none')
   	  {
-  	    $('#student_panel').toggle();
   	    $('#ta_panel').toggle();
-  	    
-  	    $('#student_tab').css('color','#aa6622');
-  	    $('#student_tab').css('text-shadow','1px 1px 1px #eeee88');
-      	$('#ta_tab').css('color','#834f1a');   
-  	    $('#ta_tab').css('text-shadow','1px 1px 1px #ccaa66');
-  	    
-  	    $('#ta_tab').hover(
-  	      function ()
-  	      {
-            $(this).css('text-shadow','1px 1px 1px #eeee88');
-          },
-          function ()
-          {
-            $(this).css('text-shadow','1px 1px 1px #ccaa66'); 
-          }	        
-  	    );
-  	    
-  	     $('#student_tab').hover(
-  	      function ()
-  	      {
-            $(this).css('text-shadow','1px 1px 1px #eeee88');
-          },
-          function ()
-          {
-            $(this).css('text-shadow','1px 1px 1px #eeee88'); 
-          }	 
-        );
-  	    
-  	    $('#ta_tab .left_tab').css(
-  	      {
-  	        'background-image' : 'url(\'/assets/htab_left_dark.png\')', 
-  	        'background-position' :  '0px 0px',
-  	        'background-repeat' : 'no-repeat'
-  	      }
-  	    );
-  	    
-  	    $('#ta_tab .content_tab').css(
-  	      {
-  	        'background-image' : 'url(\'/assets/h_wood_dark.jpg\')',
-  	        'background-position' : '0px 0px',
-  	        'background-repeat' : 'repeat-x'
-  	      }
-  	    );
-  	    
-  	    $('#ta_tab .right_tab').css(
-  	      {
-  	        'background-image' : 'url(\'/assets/htab_right_dark.png\')',
-  	        'background-position' : '0px 0px',
-  	        'background-repeat' : 'no-repeat'
-  	      }
-  	    );
-  	    
-  	    
-  	    $('#student_tab .left_tab').css(
-  	      {
-  	        'background-image' : 'url(\'/assets/htab_left.png\')',
-  	        'background-position' : '0px 0px',
-  	        'background-repeat' : 'no-repeat'
-  	      }
-  	    );
-  	    
-  	    $('#student_tab .content_tab').css(
-  	      {
-  	        'background-image' : 'url(\'/assets/h_wood.jpg\')',
-  	        'background-position' : '0px 0px',
-  	        'background-repeat' : 'repeat-x'
-  	      }
-  	    );
-  	    
-  	    $('#student_tab .right_tab').css(
-  	      {
-  	        'background-image' : 'url(\'/assets/htab_right.png\')',
-  	        'background-position' : '0px 0px',
-  	        'background-repeat' : 'no-repeat'
-  	      }
-  	    );
-  	    
+        $('#student_panel').toggle();
+  	    turnOnStudentTab(); 
   	  }
   	  
   	  if ($(this).attr('id') == 'ta_tab' && $('#ta_panel').css('display') == 'none')
   	  {
-  	    $('#student_panel').toggle();
-  	    $('#ta_panel').toggle();
-  	    
-  	    $('#student_tab').css('color','#834f1a');
-  	    $('#student_tab').css('text-shadow','1px 1px 1px #ccaa66');
-      	$('#ta_tab').css('color','#aa6622');   
-  	    $('#ta_tab').css('text-shadow','1px 1px 1px #eeee88');
-  	    
-  	    $('#student_tab').hover(
-  	      function ()
-  	      {
-            $(this).css('text-shadow','1px 1px 1px #eeee88');
-          },
-          function ()
-          {
-            $(this).css('text-shadow','1px 1px 1px #ccaa66'); 
-          }	        
-  	    );
-  	    
-  	    $('#ta_tab').hover(
-  	      function ()
-  	      {
-            $(this).css('text-shadow','1px 1px 1px #eeee88');
-          },
-          function ()
-          {
-            $(this).css('text-shadow','1px 1px 1px #eeee88'); 
-          }	 
-        );
-  	    
-  	    $('#ta_tab .left_tab').css(
-  	      {
-  	        'background-image' : 'url(\'/assets/htab_left.png\')', 
-  	        'background-position' :  '0px 0px',
-  	        'background-repeat' : 'no-repeat'
-  	      }
-  	    );
-  	    
-  	    $('#ta_tab .content_tab').css(
-  	      {
-  	        'background-image' : 'url(\'/assets/h_wood.jpg\')',
-  	        'background-position' : '0px 0px',
-  	        'background-repeat' : 'repeat-x'
-  	      }
-  	    );
-  	    
-  	    $('#ta_tab .right_tab').css(
-  	      {
-  	        'background-image' : 'url(\'/assets/htab_right.png\')',
-  	        'background-position' : '0px 0px',
-  	        'background-repeat' : 'no-repeat'
-  	      }
-  	    );  	    
-  	    
-  	    $('#student_tab .left_tab').css(
-  	      {
-  	        'background-image' : 'url(\'/assets/htab_left_dark.png\')',
-  	        'background-position' : '0px 0px',
-  	        'background-repeat' : 'no-repeat'
-  	      }
-  	    );
-  	    
-  	    $('#student_tab .content_tab').css(
-  	      {
-  	        'background-image' : 'url(\'/assets/h_wood_dark.jpg\')',
-  	        'background-position' : '0px 0px',
-  	        'background-repeat' : 'repeat-x'
-  	      }
-  	    );
-  	    
-  	    $('#student_tab .right_tab').css(
-  	      {
-  	        'background-image' : 'url(\'/assets/htab_right_dark.png\')',
-  	        'background-position' : '0px 0px',
-  	        'background-repeat' : 'no-repeat'
-  	      }
-  	    );
+  	   	$('#student_panel').toggle();
+        $('#ta_panel').toggle();
+        turnOnTaTab();
   	  }	  
   	}
 	);
+	
+	function turnOnTaTab ()
+	{    
+    $('#student_tab').css('color','#834f1a');
+    $('#student_tab').css('text-shadow','1px 1px 1px #ccaa66');
+  	$('#ta_tab').css('color','#aa6622');   
+    $('#ta_tab').css('text-shadow','1px 1px 1px #eeee88');
+    
+    $('#student_tab').hover(
+      function ()
+      {
+        $(this).css('text-shadow','1px 1px 1px #eeee88');
+      },
+      function ()
+      {
+        $(this).css('text-shadow','1px 1px 1px #ccaa66'); 
+      }	        
+    );
+    
+    $('#ta_tab').hover(
+      function ()
+      {
+        $(this).css('text-shadow','1px 1px 1px #eeee88');
+      },
+      function ()
+      {
+        $(this).css('text-shadow','1px 1px 1px #eeee88'); 
+      }	 
+    );
+    
+    $('#ta_tab .left_tab').css(
+      {
+        'background-image' : 'url(\'/assets/htab_left.png\')', 
+        'background-position' :  '0px 0px',
+        'background-repeat' : 'no-repeat'
+      }
+    );
+    
+    $('#ta_tab .content_tab').css(
+      {
+        'background-image' : 'url(\'/assets/h_wood.jpg\')',
+        'background-position' : '0px 0px',
+        'background-repeat' : 'repeat-x'
+      }
+    );
+    
+    $('#ta_tab .right_tab').css(
+      {
+        'background-image' : 'url(\'/assets/htab_right.png\')',
+        'background-position' : '0px 0px',
+        'background-repeat' : 'no-repeat'
+      }
+    );  	    
+    
+    $('#student_tab .left_tab').css(
+      {
+        'background-image' : 'url(\'/assets/htab_left_dark.png\')',
+        'background-position' : '0px 0px',
+        'background-repeat' : 'no-repeat'
+      }
+    );
+    
+    $('#student_tab .content_tab').css(
+      {
+        'background-image' : 'url(\'/assets/h_wood_dark.jpg\')',
+        'background-position' : '0px 0px',
+        'background-repeat' : 'repeat-x'
+      }
+    );
+    
+    $('#student_tab .right_tab').css(
+      {
+        'background-image' : 'url(\'/assets/htab_right_dark.png\')',
+        'background-position' : '0px 0px',
+        'background-repeat' : 'no-repeat'
+      }
+    );
+	}
+	
+	function turnOnStudentTab ()
+	{    
+    $('#student_tab').css('color','#aa6622');
+    $('#student_tab').css('text-shadow','1px 1px 1px #eeee88');
+  	$('#ta_tab').css('color','#834f1a');   
+    $('#ta_tab').css('text-shadow','1px 1px 1px #ccaa66');
+    
+    $('#ta_tab').hover(
+      function ()
+      {
+        $(this).css('text-shadow','1px 1px 1px #eeee88');
+      },
+      function ()
+      {
+        $(this).css('text-shadow','1px 1px 1px #ccaa66'); 
+      }	        
+    );
+    
+     $('#student_tab').hover(
+      function ()
+      {
+        $(this).css('text-shadow','1px 1px 1px #eeee88');
+      },
+      function ()
+      {
+        $(this).css('text-shadow','1px 1px 1px #eeee88'); 
+      }	 
+    );
+    
+    $('#ta_tab .left_tab').css(
+      {
+        'background-image' : 'url(\'/assets/htab_left_dark.png\')', 
+        'background-position' :  '0px 0px',
+        'background-repeat' : 'no-repeat'
+      }
+    );
+    
+    $('#ta_tab .content_tab').css(
+      {
+        'background-image' : 'url(\'/assets/h_wood_dark.jpg\')',
+        'background-position' : '0px 0px',
+        'background-repeat' : 'repeat-x'
+      }
+    );
+    
+    $('#ta_tab .right_tab').css(
+      {
+        'background-image' : 'url(\'/assets/htab_right_dark.png\')',
+        'background-position' : '0px 0px',
+        'background-repeat' : 'no-repeat'
+      }
+    );
+    
+    
+    $('#student_tab .left_tab').css(
+      {
+        'background-image' : 'url(\'/assets/htab_left.png\')',
+        'background-position' : '0px 0px',
+        'background-repeat' : 'no-repeat'
+      }
+    );
+    
+    $('#student_tab .content_tab').css(
+      {
+        'background-image' : 'url(\'/assets/h_wood.jpg\')',
+        'background-position' : '0px 0px',
+        'background-repeat' : 'repeat-x'
+      }
+    );
+    
+    $('#student_tab .right_tab').css(
+      {
+        'background-image' : 'url(\'/assets/htab_right.png\')',
+        'background-position' : '0px 0px',
+        'background-repeat' : 'no-repeat'
+      }
+    );
+	}
 	
 	// Default form values and input responses to focus in/out
 	$('#student_username, #ta_username')
@@ -233,6 +250,17 @@ $(document).ready(function()
       if ($(this).attr('value') == '')
       {
         $(this).attr('value','username');
+        $(this).css('color','#666');
+      }
+      
+      if ($(this).attr('value') != 'username')
+      {
+        $(this).css('color','#fff');
+      }
+      
+      if ($(this).attr('value') == 'username')
+      {
+        $(this).css('color','#666');
       }
     }
   );
@@ -244,29 +272,38 @@ $(document).ready(function()
       {
         $(this).attr('value','location');
       }
+      
+      if ($(this).attr('value') != 'location')
+      {
+        $(this).css('color','#fff');
+      }
+      
+      if ($(this).attr('value') == 'location')
+      {
+        $(this).css('color','#666');
+      }
     }
   );
   
   $('#ta_password')
     .focusout(function () 
     {
-      if ($(this).attr('value') == '')
+      if ($(this).attr('value') == '' || $(this).attr('value') == 'password')
       {
         document.getElementById('ta_password').setAttribute('type','text');
         $(this).attr('value','password');
       }
+      
+      if ($(this).attr('value') != 'password')
+      {
+        $(this).css('color','#fff');
+      }
+      
+      if ($(this).attr('value') == 'password')
+      {
+        $(this).css('color','#666');
+      }
     }
   );
-  
-  $('#new_ta').submit(function ()
-  {
-    alert('here');
-    window.location.search = 'ta=true';
-  });
-  
-  $('#new_student').submit(function ()
-  {
-    
-  });
   
 });
